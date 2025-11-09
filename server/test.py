e8f34ea58063b718427caf31e74f8b749be4eace
vishal230404 <ce220004050@iiti.ac.in>
2025-11-10 03:45:53 +0530

    upload to HF

--- server/test.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.models import load_model
-
-try:
-    model = load_model("my_model.h5")
-    print("✅ Model loaded successfully!")
-except Exception as e:
-    print("❌ Model failed to load:", e)
